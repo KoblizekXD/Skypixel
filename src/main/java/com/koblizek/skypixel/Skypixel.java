8df8af35ea1e4bde4265e0e23c192c133c1e9fc1
KoblizekXD <janprokupek@centrum.cz>
2022-09-16 20:08:19 +0200

    Initial commit

---
+++ src/main/java/com/koblizek/skypixel/Skypixel.java
@@ -0,0 +1,16 @@
+package com.koblizek.skypixel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Skypixel extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
